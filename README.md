3b957200a92fa0e14791ad12f83e957762c4cdf5
davidliu <dlliu@ucsd.edu>
2021-11-14 22:08:12 -0800

    homework

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 8 - Starter
+## Name: David Liu
+1. Within a Github action that runs whenever code is pushed. This is because you want the codes to be correct before it is pushed in the repository, and the code should only be allowed to push when it passes all of the automated tests.
+2. No. 
+3. No, beucase sending a message is considered a feature that incorporates multiple functions to correctly produce the requirements, and that should be handled by E2E testing instead of unit testing.
+4. Yes, because this can be achieved through a single function, and unit tests are specifically geared towards small scale individual function testing. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,69 @@
 // unit.test.js
 
+const { test, expect } = require('@jest/globals');
 const functions = require('../code-to-unit-test/unit-test-me.js');
+test('isPhoneNumber test1', () => {
+    expect(functions.isPhoneNumber('(232)-343-3232')).toBe(true);
+});
+test('isPhoneNumber test2', () => {
+    expect(functions.isPhoneNumber('(111)-111-1111')).toBe(true);
+});
+test('isPhoneNumber test3', () => {
+    expect(functions.isPhoneNumber('1234567890')).toBe(false);
+});
+test('isPhoneNumber test4', () => {
+    expect(functions.isPhoneNumber('')).toBe(false);
+});
 
+test('isEmail test1', () => {
+    expect(functions.isEmail('randomjoe@hotmail.com')).toBe(true);
+});
+test('isEmail test2', () => {
+    expect(functions.isEmail('fakeperson@gmail.com')).toBe(true);
+});
+test('isEmail test3', () => {
+    expect(functions.isEmail('')).toBe(false);
+});
+test('isEmail test4', () => {
+    expect(functions.isEmail('randomjoemama')).toBe(false);
+});
+
+test('isStrongPassword test1', () => {
+    expect(functions.isStrongPassword('aaaa')).toBe(true);
+});
+test('isStrongPassword test2', () => {
+    expect(functions.isStrongPassword('a000')).toBe(true);
+});
+test('isStrongPassword test3', () => {
+    expect(functions.isStrongPassword('')).toBe(false);
+});
+test('isStrongPassword test4', () => {
+    expect(functions.isStrongPassword('1032')).toBe(false);
+});
+
+test('isDate test1', () => {
+    expect(functions.isDate('10/32/1111')).toBe(true);
+});
+test('isDate test2', () => {
+    expect(functions.isDate('1/32/1111')).toBe(true);
+});
+test('isDate test3', () => {
+    expect(functions.isDate('')).toBe(false);
+});
+test('isDate test4', () => {
+    expect(functions.isDate('10/323/1111')).toBe(false);
+});
+
+test('isHexColor test1', () => {
+    expect(functions.isHexColor('#00ffff')).toBe(true);
+});
+test('isHexColor test2', () => {
+    expect(functions.isHexColor('#ff0000')).toBe(true);
+});
+test('isHexColor test3', () => {
+    expect(functions.isHexColor('')).toBe(false);
+});
+test('isHexColor test4', () => {
+    expect(functions.isHexColor('#fgdgfg')).toBe(false);
+});
 // TODO - Part 2
